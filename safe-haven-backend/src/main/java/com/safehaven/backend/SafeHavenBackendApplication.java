4e81e7db0300c88f80d4c7367fb706ba471e6d74
BabisDros <150071850+BabisDros@users.noreply.github.com>
2026-04-30 11:44:44 +0300

    feat (backend): project setup

---
+++ safe-haven-backend/src/main/java/com/safehaven/backend/SafeHavenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safehaven.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHavenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeHavenBackendApplication.class, args);
+	}
+
+}
---
+++ safe-haven-backend/src/test/java/com/safehaven/backend/SafeHavenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safehaven.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeHavenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
